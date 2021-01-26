a96964c2d3ee813895745e6cadce24cbb2a5b88f
Pedro Azeredo <pedro.azeredo93@gmail.com>
2021-01-26 06:02:09 -0300

    Utilizando class Component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Equipe = (props) => {
-    return(
-        <div>
-            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} salario={props.salario} />
-            <Social gh={props.github}/>
-        </div>
-    );
+class Equipe extends Component{
+    render(){
+        return(
+            <div>
+                <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} salario={this.props.salario}/>
+                <hr/>
+            </div>
+        
+        )
+    }
 }
 
-const Sobre = (props) =>{
-    return(
-        <div>
-            <h2>Olá sou o(a) {props.nome}</h2>
-            <h3>Cargo: {props.cargo}</h3>
-            <h3>Idade: {props.idade}</h3>
-            <h3>Salario: {props.salario}</h3>
+class Sobre extends Component{
+    render(){
+        return(
+            <div>
+            <h2>Olá sou o(a) {this.props.nome}</h2>
+            <h3>Cargo: {this.props.cargo}</h3>
+            <h3>Idade: {this.props.idade}</h3>
+            <h3>Salario: {this.props.salario}</h3>
         </div>
-    );
-}
-const Social = (props) =>{
-    return(
-        <div>
-           <a href={props.gh}>GitHub</a>
-           <a>Linkedin</a>
-           <a>Email</a>
-        </div>
-    );
+        )
+    }
 }
 
 function App(){
     return (
         <div>
            <h1>Conheça nossa equipe: </h1>
-           <Equipe nome="Pedro" cargo="Desenvolvedor" idade="21" salario="R$100.000,00" github="https://github.com/"/>
+           <Equipe nome="Pedro" cargo="Desenvolvedor" idade="21" salario="R$100.000,00"/>
            <Equipe nome="Francesca" cargo="Diretora Geral" idade="22" salario="R$10.000,00"/>
-           <Equipe nome="Melissa" cargo="Tech Recuiter" idade="28" salario="R$5.000,00"/>
+           <Equipe nome="Melissa" cargo="Tech Recruiter" idade="28" salario="R$5.000,00"/>
         </div>
     );
 }
