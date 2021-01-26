f97ab46ffeb6cb54d02098f865839fbd4515920d
Pedro Azeredo <pedro.azeredo93@gmail.com>
2021-01-26 06:31:52 -0300

    Brincando com (states)

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
 import React, { Component } from 'react';
 
-class Equipe extends Component{
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            nome: 'Pedro',
+            contador: 0
+        };
+
+        this.aumentar = this.aumentar.bind(this);
+        this.diminuir = this.diminuir.bind(this);
+    }
+
+    aumentar(){
+        let state = this.state;
+        state.contador += 1;
+        state.nome = 'Francesca';
+        this.setState(state);
+    }
+
+    diminuir(){
+        let state = this.state;
+        if(this.contador == 0){
+            alert('Opa chegou a zero');
+            return;
+        }
+        state.contador -= 1;
+        this.setState(state);
+    }
+
     render(){
         return(
             <div>
-                <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} salario={this.props.salario}/>
-                <hr/>
+                <h3>Contandor</h3>
+                {this.state.nome} 
+                <h3>
+                    <button onClick={this.diminuir} class="btn">
+                            -
+                    </button>
+                    {this.state.contador}
+                    <button onClick={this.aumentar} class="btn">
+                            +
+                    </button>
+                </h3>
             </div>
-        
-        )
+        );
     }
-}
-
-class Sobre extends Component{
-    render(){
-        return(
-            <div>
-            <h2>Olá sou o(a) {this.props.nome}</h2>
-            <h3>Cargo: {this.props.cargo}</h3>
-            <h3>Idade: {this.props.idade}</h3>
-            <h3>Salario: {this.props.salario}</h3>
-        </div>
-        )
-    }
-}
-
-function App(){
-    return (
-        <div>
-           <h1>Conheça nossa equipe: </h1>
-           <Equipe nome="Pedro" cargo="Desenvolvedor" idade="21" salario="R$100.000,00"/>
-           <Equipe nome="Francesca" cargo="Diretora Geral" idade="22" salario="R$10.000,00"/>
-           <Equipe nome="Melissa" cargo="Tech Recruiter" idade="28" salario="R$5.000,00"/>
-        </div>
-    );
 }
 
 export default App;
