221bec80224ef0a0facc6e6b704ddcdad47c1deb
Pedro Azeredo <pedro.azeredo93@gmail.com>
2021-01-28 06:47:31 -0300

    Renderização condicional utilizando ternario

--- src/App.js
+++ src/App.js
@@ -5,18 +5,34 @@ class App extends Component{
     constructor(props){
         super(props);
         this.state = {
-            status: 1
+            status: true
         };
+
+        this.sair = this.sair.bind(this);
+        this.entrar = this.entrar.bind(this);
     }
+
+    sair(){
+        this.setState({status: false});
+    }
+
+    entrar(){
+        this.setState({status: true});
+    }
+
     render(){
         return(
             <div>
-                 {this.state.status === 1 &&
-                    <h1>Bem vindo ao sistema!</h1>
+                 {this.state.status ?
+                    <div>
+                        <h2>Bem vindo ao sistema!</h2>
+                        <button onClick = {this.sair}>Sair</button>
+                    </div>:
+                    <div>
+                        <h2>Olá visitante, faça seu login</h2>
+                        <button onClick = {this.entrar}>Entrar</button>
+                    </div>
                  }
-                 <div>
-                     <h2>Curso ReactJs</h2>
-                 </div>
             </div>
         );
     }
